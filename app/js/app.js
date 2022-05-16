c3e10d4337ee313481ce2eb5183c5b0a4151580a
saals <salekser@gmail.com>
2022-05-16 21:26:07 +0300

    js search, adaptive

--- app/js/app.js
+++ app/js/app.js
@@ -3,7 +3,27 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-  // Custom JS
-  // $('.header').hide();
+  $('.logo-letter').each(function () {
+    const ths = $(this);
+    ths.html(ths.html().replace('o', '<span>o</span>'));
+  });
+
+  $('.search-page__icon').click(function () {
+    $('.search-page__field').stop().slideToggle();
+    $('.search-page__field input[type=text]').focus();
+  });
+
+  $(document).keyup(function (e) {
+    if (e.keyCode === 27) {
+      $('.search-page__field').slideUp();
+    }
+  }).click(function () {
+    $('.search-page__field').slideUp();
+  });
+
+  $('.search-page').click(function (e) {
+    e.stopPropagation();
+  });
 
 });
+
